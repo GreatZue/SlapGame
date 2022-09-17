da6f521d807cf3d2a80828ab984967323909127d
GreatZue <greatzue@gmail.com>
2022-09-16 19:37:16 -0600

    commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let playerHP = document.getElementById("playerHP").innerHTML;
+let enemyHP = document.getElementById("enemyHP").innerHTML;
+
+
+
+function slap() {
+console.log(playerHP)
+
+}
+
+function punch() {
+
+}
+
+function kick() {
+
+}
