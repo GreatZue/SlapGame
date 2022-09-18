1dcabd7d560e55e485ce157bda1b43b72a7360cf
GreatZue <greatzue@gmail.com>
2022-09-17 22:07:46 -0600

    logic hookup

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-let playerHP = document.getElementById("playerHP").innerHTML;
-let enemyHP = document.getElementById("enemyHP").innerHTML;
-
-
+let playerHP = document.getElementById("playerHP").innerHTML
+let enemyHP = document.getElementById("enemyHP").innerHTML
 
 function slap() {
-console.log(playerHP)
-
+enemyHP -= 5
+console.log(enemyHP)
 }
 
 function punch() {
-
+enemyHP -= 10
+console.log(enemyHP)
 }
 
 function kick() {
-
+enemyHP -= 15
+console.log(enemyHP)
 }
+
+
+// enemyHP = parseInt(enemyHP, 10)
