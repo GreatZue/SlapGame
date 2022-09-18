fa6780b0d6a4ac7793151042a2b08e8c39eb10a1
GreatZue <greatzue@gmail.com>
2022-09-17 22:36:13 -0600

    commit

--- index.js
+++ index.js
@@ -1,20 +1,61 @@
 let playerHP = document.getElementById("playerHP").innerHTML
 let enemyHP = document.getElementById("enemyHP").innerHTML
 
+
 function slap() {
 enemyHP -= 5
-console.log(enemyHP)
+bossAttack()
+console.log(enemyHP, playerHP)
+healthCheck()
 }
 
 function punch() {
 enemyHP -= 10
-console.log(enemyHP)
+bossAttack()
+console.log(enemyHP, playerHP)
+healthCheck()
 }
 
 function kick() {
 enemyHP -= 15
-console.log(enemyHP)
+bossAttack()
+console.log(enemyHP, playerHP)
+healthCheck()
 }
 
 
-// enemyHP = parseInt(enemyHP, 10)
+
+// ---------------------------------------------------------------------------------------------//
+// ---------------------------------------------------------------------------------------------//
+// ---------------------------------------------------------------------------------------------//
+
+function healthCheck () {
+    if (enemyHP >= 1 && playerHP >= 1) {
+        document.getElementById("playerHP").innerHTML = playerHP
+        document.getElementById("enemyHP").innerHTML = enemyHP
+        return
+    }
+    else if (playerHP <= 0) {
+        document.getElementById("playerHP").innerHTML = playerHP
+        document.getElementById("enemyHP").innerHTML = enemyHP
+        alert("You lose!")
+    }
+    else {
+        document.getElementById("playerHP").innerHTML = playerHP
+        document.getElementById("enemyHP").innerHTML = enemyHP
+        alert ("You defeated!")
+    }
+}
+
+function randomIntGen(max) {
+    return Math.floor(Math.random() * max)
+}
+
+function bossAttack() {
+    playerHP -= randomIntGen(15)
+}
+
+function gameReset() {
+    document.getElementById("playerHP").innerHTML = "100"
+    document.getElementById("enemyHP").innerHTML = "100"
+}
